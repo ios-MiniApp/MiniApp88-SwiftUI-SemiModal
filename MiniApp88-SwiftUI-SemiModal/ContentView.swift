32801aedc06095637ce71044f410ef01ade956dd
Kota <maedakoutaa@gmail.com>
2022-08-18 15:08:35 +0900

    Initial Commit

---
+++ MiniApp88-SwiftUI-SemiModal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp88-SwiftUI-SemiModal
+//
+//  Created by 前田航汰 on 2022/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp88-SwiftUI-SemiModal/MiniApp88_SwiftUI_SemiModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp88_SwiftUI_SemiModalApp.swift
+//  MiniApp88-SwiftUI-SemiModal
+//
+//  Created by 前田航汰 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp88_SwiftUI_SemiModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
